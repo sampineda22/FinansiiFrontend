854ec7ef9961332d69506d1db64f31b6db724ec4
sampineda22 <114605420+sampineda22@users.noreply.github.com>
2024-05-13 13:19:27 -0600

    2024-Mayo-05 | Update #4

--- src/app/app.resolvers.ts
+++ src/app/app.resolvers.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { forkJoin, Observable } from 'rxjs';
 import { NavigationService } from 'app/core/navigation/navigation.service';
 
 @Injectable({
     providedIn: 'root'
 })
-export class InitialDataResolver implements Resolve<any>
+export class InitialDataResolver 
 {
     /**
      * Constructor
